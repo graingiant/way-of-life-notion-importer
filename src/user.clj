9a51e10ed201bb0a8af6ff7df12ffd69ed34f870
Alex Caza <alex@alexcaza.com>
2022-03-05 15:22:06 -0500

    Use doseq to update through Notion API
    TODO:
    - Convert hashmap to something dynamically generated based on column position

--- src/user.clj
+++ src/user.clj
@@ -1,9 +1,7 @@
 (ns user
   (:require [clj-http.client :as client]
-            [clojure.string :as str]
             [clojure.data.csv :as csv]
             [clojure.java.io :as io]
-            [clojure.pprint :refer [pprint]]
             [dotenv :refer [env]]))
 
 (def notion-key (str "Bearer " (env :NOTION_KEY)))
@@ -12,11 +10,6 @@
 
 (def create-page-url
   "https://api.notion.com/v1/pages")
-
-(defn db-query-url [] (str/replace "https://api.notion.com/v1/databases/:db_id/query" #":db_id" notion-database-id))
-
-(def notion-page {:parent {:type "page_id"
-                           :page_id "4cba2794330d42b48f79738edc82cad7"}})
 
 (def new-entry {:parent {:type "database_id" :database_id notion-database-id}
                 :properties {:Date {:title [{:type "text"
@@ -55,12 +48,9 @@
     (doall
      (csv/read-csv reader))))
 
-(defn get-headers [file-data]
-  (first file-data))
-
 (def csv-data (rest (parse-data)))
 
-(def csv-headers (get-headers (parse-data)))
+(def csv-headers (first (parse-data)))
 
 (defn update-title [item template]
   (assoc-in template [:properties :Date :title 0 :text :content] item))
@@ -68,14 +58,13 @@
 (defn update-rest [item title template]
   (assoc-in template [:properties title :rich_text 0 :text :content] item))
 
+;; create new collection with parsed data using update-template
 (defn update-template [row]
   (reduce-kv (fn [template idx row-item] (if (is-date-column-value idx)
                                            (update-title row-item template)
                                            (update-rest row-item (csv-headers idx) template))) new-entry row))
 
-;; create new collection with parsed data using update-template
-(pprint (first (map update-template csv-data)))
-
-(def new-entry-filled (assoc-in notion-headers [:form-params] (first (map update-template csv-data))))
-
-;; (client/post create-page-url new-entry-filled)
+(doseq [v (map update-template csv-data)]
+  (let [new-entry-filled (assoc-in notion-headers [:form-params] v)]
+    (client/post create-page-url new-entry-filled)
+    (Thread/sleep 2000)))
